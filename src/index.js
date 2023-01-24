0e43233f8877e7b630ebd62cff09cb209286bcaa
Сергей (DonStacky) <rodnihsergey1994@mail.ru>
2023-01-24 09:57:19 +0300

    feat: finish

--- src/index.js
+++ src/index.js
@@ -38,7 +38,44 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let word = [];
+    let arr = [];
+    let sentence = '';
+    for (let i = 0; i < expr.length; i += 10) {
+    arr.push(expr.slice(i, i + 10));
+    }
+    arr.map(function(item, index, array) {
+      let arrNum = [];
+      let arrDot = [];
+      let str = '';
+      for (let i = 0; i < 10; i += 2) {
+        arrNum.push(item.slice(i, i+2));
+      }
+      arrDot = arrNum.reduce((acc, value) => {
+        if (value === '00') {
+          return acc;
+        } else if (value === '10') {
+          return acc = [...acc, '.']
+        } else if (value === '11') {
+          return acc = [...acc, '-']
+        } else if (value === '**') {
+          return acc = [' ']
+        }
+      }, acc = [])
+      str = arrDot.join('');
+      word.push(str);
+    })
+    for (letter of word) {
+      if (letter === ' ') {
+        sentence = sentence + ' ';
+      } else {
+    for (let key in MORSE_TABLE) {
+      if (key === letter) {
+        sentence = sentence + MORSE_TABLE[key];
+      }
+    }}
+    }
+    return sentence;
 }
 
 module.exports = {
